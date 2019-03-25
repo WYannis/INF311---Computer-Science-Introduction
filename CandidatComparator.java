495a28f5e8d310ef772d84c686199c810db33f5c
WY <43731264+WYannis@users.noreply.github.com>
2019-03-25 18:17:08 +0100

    Revert "TD5 (5th session of practical work)"

--- CandidatComparator.java
+++
@@ -1,4 +0,0 @@
-import java.util.Comparator;
-
-public interface CandidatComparator extends Comparator<Candidat> {
-}
--- RadixSortAdapter.java
+++
@@ -1,6 +0,0 @@
-
-public interface RadixSortAdapter {
-	int getNumberOfDigits();
-	
-	CountingSortAdapter getCountingSortAdapter(int pos);
-}
